7b8b5f608fa28978cfd790de48bb56178abdb2a6
qwertxzy <leo@qwertxzy.me>
2023-09-20 19:49:21 +0200

    Cleanup

--- TextUtils.py
+++ TextUtils.py
@@ -61,15 +61,4 @@ class Scroller:
     '''Sets a new message to be displayed next.'''
     self.__switch_msg = True
     self.__next_msg = msg
-
-charset_path = 'charset.png'
-charset_lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz°1234567890@'
-host = "127.0.0.1"
-port = 6969
-
-sc = Scroller(charset_path, charset_lookup, host, port, debug=True)
-sc.set_message("peepee")
-import time
-while True:
- sc.tick()
- time.sleep(0.1)
+    
---
+++ test_textutils.py
@@ -0,0 +1,13 @@
+from TextUtils import Scroller
+
+charset_path = 'charset.png'
+charset_lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz°1234567890@'
+host = "127.0.0.1"
+port = 6969
+
+sc = Scroller(charset_path, charset_lookup, host, port, debug=True)
+sc.set_message("peepee")
+import time
+while True:
+ sc.tick()
+ time.sleep(0.1)
